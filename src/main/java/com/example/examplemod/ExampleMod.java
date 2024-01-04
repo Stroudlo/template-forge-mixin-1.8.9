f65f6c3fe0d896847f2b137432227f205d145e21
Stroudlo <155658718+Stroudlo@users.noreply.github.com>
2024-01-04 09:39:42 -0700

    Basic changes for my radium port

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -6,8 +6,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "Wind client";
+    public static final String VERSION = "0.0.1Alpha";
 
     /**
      * If everything goes to plan, you should see this message in the console.
